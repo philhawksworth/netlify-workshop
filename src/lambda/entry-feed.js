d5661b16727ebe94b6d6213e17f3ca98584eccf7
Phil Hawksworth <phil@netify.com>
2018-01-30 12:16:46 +0000

    api access config

---
+++ src/lambda/entry-feed.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var request = require("request");
+var config = require("dotenv").config();
+var FORM_ID = process.env.form_id_raffle_smashing_london_2018 || config.form_id_raffle_smashing_london_2018;
+var API_AUTH = process.env.form_api_access_token || config.form_api_access_token;
+
+export function handler(event, context, callback) {
+
+  // define the desired URL
+  var url = "https://api.netlify.com/api/v1/submissions/" + FORM_ID + "?access_token=" + API_AUTH;
+
+  request(url, function(err, response, body){
+    if(!err && response.statusCode === 200){
+      console.log("...we got a result");
+      callback(null, {
+        statusCode: 200,
+        body: body
+      })
+    }
+  });
+}
