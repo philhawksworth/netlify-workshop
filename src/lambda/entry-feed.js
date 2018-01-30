1ff204af1e11ca062186d9d649bfc2e1bf28a319
Phil Hawksworth <phil@netify.com>
2018-01-30 13:01:10 +0000

    safely get entry data

--- src/lambda/entry-feed.js
+++ src/lambda/entry-feed.js
@@ -8,14 +8,21 @@ var API_AUTH = process.env.form_api_access_token || config.form_api_access_token
 export function handler(event, context, callback) {
 
   // define the desired URL
-  var url = "https://api.netlify.com/api/v1/submissions/" + FORM_ID + "?access_token=" + API_AUTH;
+  var url = "https://api.netlify.com/api/v1/forms/" + FORM_ID + "/submissions/?access_token=" + API_AUTH;
+  console.log("Requesting", url);
 
   request(url, function(err, response, body){
     if(!err && response.statusCode === 200){
-      console.log("...we got a result");
+      var results = [];
+      var formsData = JSON.parse(body);
+      for(var item in formsData) {
+        results.push({
+          "twitterHandle": formsData[item].data.twitter
+        });
+      }
       return callback(null, {
         statusCode: 200,
-        body: body
+        body: JSON.stringify(results)
       })
     } else {
       return callback(null, {
