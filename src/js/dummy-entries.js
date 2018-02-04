c553e8627ae9cbceb63c6c330674e325444a9808
Phil Hawksworth <phil@netify.com>
2018-02-04 18:26:24 +0000

    mock data

--- src/js/dummy-entries.js
+++ src/js/dummy-entries.js
@@ -1,16 +1,3 @@
 {
-  "items" : [
-    { "twitterHandle": "one"},
-    { "twitterHandle": "two"},
-    { "twitterHandle": "three"},
-    { "twitterHandle": "four"},
-    { "twitterHandle": "five"},
-    { "twitterHandle": "six"},
-    { "twitterHandle": "seven"},
-    { "twitterHandle": "eight"},
-    { "twitterHandle": "nine"},
-    { "twitterHandle": "ten"},
-    { "twitterHandle": "eleven"},
-    { "twitterHandle": "twelve"}
-  ]
+  handles : ["philhawksworth", "netlify", "netlifyCMS", "netlifyStatus"]
 }
