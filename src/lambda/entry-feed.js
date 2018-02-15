1d1978b566584858db5a65c1fb582a4a5fee8ebd
Phil Hawksworth <phil@netify.com>
2018-02-15 10:47:16 +0000

    functions tidy for better dotenv use

--- src/lambda/entry-feed.js
+++ src/lambda/entry-feed.js
@@ -8,15 +8,13 @@ export function handler(event, context, callback) {
 
   // select the form ID environment variable depending on what we requested
   var form_id = "form_id_" + event.queryStringParameters['form_name'];
-  var FORM_ID = process.env[form_id] || config[form_id];
-
-  console.log(form_id );
-
-  var API_AUTH = process.env.form_api_access_token || config.form_api_access_token;
+  var FORM_ID = process.env[form_id];
+  var API_AUTH = process.env.form_api_access_token;
 
   // define the desired URL
   var url = "https://api.netlify.com/api/v1/forms/" + FORM_ID + "/submissions/?access_token=" + API_AUTH;
   console.log("Requesting", url);
+
   request(url, function(err, response, body){
 
     // format the response to be a bit mor concise and return it to the client
@@ -24,15 +22,18 @@ export function handler(event, context, callback) {
       var results = [];
       var formsData = JSON.parse(body);
       for(var item in formsData) {
+        // normalize things. We don;t care about @s
         var handle = formsData[item].data.twitter.replace("@", "");
         if(handle.length > 0) {
           console.log("Entry found: ", handle)
           results.push(handle);
         }
       }
+      // Create a set of results with no dupes
       var data = {
         "handles" : [...new Set(results)]
       };
+      // send it
       return callback(null, {
         statusCode: 200,
         headers: {"Content-Type": "application/json"},
